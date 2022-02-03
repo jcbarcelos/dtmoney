58e0b7511f30279b56b8e718d96aaada8ca8f16f
Júlio César <jcbarcelos>
2022-02-03 16:53:46 -0300

    feat: ensure newModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
     useState(false);
@@ -18,12 +19,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModalOpen}
-      >
-        <h2>Cadastrar</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
