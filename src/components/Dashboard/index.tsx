230313c0af2b8260485bdd18d2d38267867e5ab3
Júlio César <jcbarcelos>
2022-02-03 11:15:50 -0300

    feat: ensure Transectiontable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
 import { Container } from "./styled";
 
 export function Dashboard() {
     return (
        <Container>
            <Summary />
+           <TransactionTable />
        </Container>
     )
 }
